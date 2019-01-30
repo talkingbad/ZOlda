6decf27b8509d6da7741df6ec5d4fd479e8a2c93
talkingbad <47176442+talkingbad@users.noreply.github.com>
2019-01-30 11:32:49 +0100

    Add files via upload

---
+++ Gameboard.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Gameboard : MonoBehaviour {
+
+    // TODO remember to change under rock to stairs
+    // after rock is moved
+    // TODO change to gem and then stump after the bush
+    // TODO the same for boom, bomb, bow , vace
+
+    // types of objects hills, trees, bush, house, boom
+    //bomb, stone, bow, vace, gem, stairs, door, chest
+
+    public string[,] gameObjects = new string[26, 19];
+
+
+	// Use this for initialization
+	void Start () {
+
+        AddYRowXRange(0, 0, 25, "Hill");
+        AddYRowXRange(18, 0, 25, "Hill");
+        AddYRowXRange(3, 0, 7, "Hill");
+        AddYRowXRange(5, 0, 6, "Hill");
+        AddYRowXRange(4, 0, 7, "Hill");
+        AddYRowXRange(5, 0, 6, "Hill");
+        AddYRowXRange(14, 0, 4, "Hill");
+        AddYRowXRange(15, 0, 4, "Hill");
+        AddYRowXRange(5, 0, 6, "Hill");
+        AddYRowXRange(15, 7, 11, "Hill");
+        AddYRowXRange(14, 7, 10, "Hill");
+        AddYRowXRange(12, 15, 22, "Hill");
+        AddYRowXRange(13, 15, 22, "Hill");
+
+        AddxColYRange(0, 0, 18, "Hill");
+        AddxColYRange(25, 0, 18, "Hill");
+        AddxColYRange(10, 0, 14, "Hill");
+        AddxColYRange(12, 0, 14, "Hill");
+
+        AddYRowXRange(17, 14, 17, "Tree");
+        AddYRowXRange(16, 14, 17, "Tree");
+        AddYRowXRange(15, 19, 20, "Tree");
+        AddYRowXRange(14, 19, 20, "Tree");
+
+        AddYRowXRange(17, 19, 22, "Bush");
+        AddYRowXRange(2, 3, 5, "Bush");
+
+        AddYRowXRange(5, 18, 20, "House");
+        AddYRowXRange(4, 18, 20, "House");
+        AddxColYRange(18, 3, 3, "House");
+        AddxColYRange(20, 3, 3, "House");
+
+        // Place single game objects
+        gameObjects[24, 1] = "bow";
+        gameObjects[1, 17] = "bomb";
+        gameObjects[8, 11] = "boomerang";
+        gameObjects[2, 1] = "stone";
+        gameObjects[19, 3] = "Door";
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddYRowXRange(int yRow, int xStart, int xEnd, string gOName)
+    {
+        for(int i = xStart; i <= xEnd; i++)
+        {
+            gameObjects[i, yRow] = gOName;
+        }
+    }
+
+    void AddxColYRange(int xColumn, int yStart, int yEnd, string gOName)
+    {
+        for (int i = yStart; i <= yEnd; i++)
+        {
+            gameObjects[xColumn, i] = gOName;
+        }
+    }
+
+    public bool isValidSpace(float x, float y, float horzMove, float vertMove)
+    {
+        x = horzMove < 0 ? x + 1 : x;
+        y = vertMove < 0 ? y + 1 : x;
+
+        x = (float)Math.Floor(Convert.ToDouble(x));
+        y = (float)Math.Floor(Convert.ToDouble(y));
+
+        if(gameObjects [(int)x, (int)y] == null)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+
+
+    }
+
+
+
+}
---
+++ Zolda.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Zolda : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    Gameboard gameBoard;
+
+    SoundManager soundManager;
+
+    Animator animator;
+
+    bool facingRight = false;
+    bool facingLeft = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gameBoard = FindObjectOfType(typeof(Gameboard)) as Gameboard;
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>
+            ();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float horzMove = Input.GetAxisRaw("Horizontal");
+        float vertMove = Input.GetAxisRaw("Vertical");
+
+        if (gameBoard.isValidSpace((float)transform.position.x + horzMove,
+            (float)transform.position.y + vertMove, horzMove, vertMove))
+        {
+            rb.velocity = new Vector2(horzMove * speed, vertMove * speed);
+
+
+        } else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            animator.Play("walkdown");
+        }
+        if (Input.GetKey("w"))
+        {
+            animator.Play("walkup");
+        }
+        if (Input.GetKey("d"))
+        {
+            animator.Play("walkright");
+
+            if (facingLeft)
+            {
+                facingRight = true;
+                facingLeft = false;
+                animator.transform.Rotate(0, 180, 0);
+            } else if (!facingLeft)
+            {
+                facingRight = true;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                animator.Play("walkright");
+                
+                if (facingRight)
+                {
+                    facingRight = false;
+                    facingLeft = true;
+                    animator.transform.Rotate(0, 180, 0);
+                }
+                else if (!facingRight)
+                {
+                    facingLeft = true;
+                }
+
+
+            }
+
+            if (Input.GetKeyUp("s"))
+            {
+                animator.Play("idle");
+
+                float centerX = (float)Math.Round(Convert.ToDouble(transform.position.x));
+                float centerY = (float)Math.Round(Convert.ToDouble(transform.position.y));
+                transform.position = new Vector2(centerX, centerY);
+            }
+            }
+
+
+
+
+
+
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            float targetaspect = 16.0f / 9.0f;
+
+            float windowaspect = (float)Screen.width / (float)Screen.height;
+
+            float scaleheight = windowaspect / targetaspect;
+
+            Camera camera = GetComponent<Camera>();
+
+            if (scaleheight < 1.0f)
+            {
+                Rect rect = camera.rect;
+
+                rect.width = 1.0f;
+                rect.height = scaleheight;
+                rect.x = 0;
+                rect.y = (1.0f - scaleheight) / 2.0f;
+
+                camera.rect = rect;
+            }
+            else // add pillarbox
+            {
+                float scalewidth = 1.0f / scaleheight;
+
+                Rect rect = camera.rect;
+
+                rect.width = scalewidth;
+                rect.height = 1.0f;
+                rect.x = (1.0f - scalewidth) / 2.0f;
+                rect.y = 0;
+
+                camera.rect = rect;
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
+
